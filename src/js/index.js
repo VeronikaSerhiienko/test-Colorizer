07a35ced315bd22cf2ad4d37e48f7cdb66b94a22
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-01-25 12:00:12 +0200

    add floating bar

--- src/js/index.js
+++ src/js/index.js
@@ -19,59 +19,67 @@
   }
 })();
 
-;(function() {
+var newScriptTag = document.createElement('script');
+newScriptTag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(newScriptTag, firstScriptTag);
+var popUpVideoPlayer;
+function onYouTubeIframeAPIReady() {
+ popUpVideoPlayer = new YT.Player('js-pop-up-video', {
+   height: '315',
+   width: '560',
+   autoplay: 1,
+   videoId: 'JwChyACAbSc',
+   events: {
+     'onReady': onPlayerReady,
+   }
+ });
+}
+
+function onPlayerReady(event) {
+  var playerReadyEvent = event;
+  playerReadyEvent.target.mute();
+  playerReadyEvent.target.playVideo();
+  playerReadyEvent.target.pauseVideo();
+  playerReadyEvent.target.unMute();
   var modalWrapper = document.querySelector('.js-pop-up-wrapper');
   var modalWindow = document.querySelector('.js-pop-up');
-  var modalVideo = document.querySelector(".js-pop-up-video");
-  var videoLink = modalVideo.getAttribute('src');
-  document.querySelector('.js-pop-up-button-open').addEventListener('click', function(event) {    
-    setTimeout(function() {
-      var videoAutoPlay = videoLink + '?autoplay=1';
-      modalVideo.setAttribute('src', videoAutoPlay);
-      console.log(modalVideo.getAttribute('src'));
-      modalWindow.classList.add('visible');
-      modalWrapper.classList.add('visible');
-    },500);
-  });
+  document.querySelector('.js-pop-up-button-open').addEventListener('click', function(event) {
+  setTimeout(function() {
+    playerReadyEvent.target.playVideo();
+    modalWindow.classList.add('visible');
+    modalWrapper.classList.add('visible');
+  },500);
+});
 
-  document.querySelector('.js-pop-up-button-close').addEventListener('click', function(event) {
-    modalWindow.classList.remove('visible');
-    modalWrapper.classList.remove('visible');
-    modalVideo.setAttribute('src', videoLink);
-  });
+ document.querySelector('.js-pop-up-button-close').addEventListener('click', function(event) {
+   modalWindow.classList.remove('visible');
+   modalWrapper.classList.remove('visible');
+   playerReadyEvent.target.pauseVideo();
+ });
 
-  modalWrapper.addEventListener('click', function(event) {
-    modalWindow.classList.remove('visible');
-    modalWrapper.classList.remove('visible');
-    modalVideo.setAttribute('src', videoLink); 
-  });
+ modalWrapper.addEventListener('click', function(event) {
+   modalWindow.classList.remove('visible');
+   modalWrapper.classList.remove('visible');
+   playerReadyEvent.target.pauseVideo();
+ });
 
-  modalWindow.addEventListener('click', function(event) {
-    event.stopPropagation();    
-  });
+ modalWindow.addEventListener('click', function(event) {
+   event.stopPropagation();
+ });
+}
+
+;(function() {
+  var beta = document.querySelector('.js-beta');
+  var originalY = document.querySelector('.js-original-colorizer').offsetTop;
+  var testingY = document.querySelector('.js-colorizer-testing').offsetTop;
+  window.onscroll = function() {
+    var pageY = window.pageYOffset || document.documentElement.scrollTop;
+
+          if (pageY >= originalY && pageY <= testingY) {
+            beta.classList.add('visible');
+          } else {
+            beta.classList.remove('visible');
+          }
+      };
 })();
-// (function() {
-//   window.document.onkeydown = function(e) {
-//   if (!e) {
-//     e = event;
-//   }
-//   if (e.keyCode == 27) {
-//     lightbox_close();
-//   }
-// };
-
-// function lightbox_open() {
-//   var lightBoxVideo = document.querySelector(".js-video");
-//   window.scrollTo(0, 0);
-//   document.querySelector('.js-pop-up').style.display = 'block';
-//   document.querySelector('.js-pop-up-wrapper').style.display = 'block';
-//   lightBoxVideo.play();
-// }
-
-// function lightbox_close() {
-//   var lightBoxVideo = document.querySelector(".js-video");
-//   document.querySelector('.js-pop-up').style.display = 'none';
-//   document.querySelector('.js-pop-up-wrapper').style.display = 'none';
-//   lightBoxVideo.pause();
-// }
-// })();
